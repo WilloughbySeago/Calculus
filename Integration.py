b12e898d9c187ee494b3bb447935f19c6a7a3423
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-04 19:19:20 +0100

    Added trapezium_rule function
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Integration.py
+++ Integration.py
@@ -5,3 +5,17 @@ from decimal import *
 
 getcontext().rounding = ROUND_HALF_UP
 getcontext().prec = 64
+
+
+def trapezium_rule(function, a, b, n):
+    h = Decimal(b - a) / n
+    y_values = []
+    x = a
+    for i in range(0, n):
+        y = function(a + i * h)
+        y_values.append(y)
+    integral = y_values.pop(0) + y_values.pop(-1)
+    integral += 2 * sum(y_values)
+    integral *= Decimal(1 / 2)
+    integral *= h
+    return integral
