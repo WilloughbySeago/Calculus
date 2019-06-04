210b09dc432b42784868d6e08c1e77cfef129806
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-04 18:34:23 +0100

    Added docstrings and increased precision
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Differentiation.py
+++ Differentiation.py
@@ -4,9 +4,18 @@ This is a collection of functions related to differentiation
 from decimal import *
 
 getcontext().rounding = ROUND_HALF_UP
+getcontext().prec = 64
 
 
 def derivative(function, x, h=Decimal(0.0000000000010000000)):
+    """
+    This function will approximate the derivative of a single variable function using:
+    dy/dx = (f(x+h) - f(x))/h
+    :param function: function (1 argument)
+    :param x: int, float, Decimal
+    :param h: int, float, Decimal
+    :return: Decimal
+    """
     f_of_x = function(Decimal(x))
     f_of_x_plus_h = function(x + h)
     dy_dx = (f_of_x_plus_h - f_of_x) / h
