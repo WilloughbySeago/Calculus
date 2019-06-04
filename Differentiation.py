a5449a282ead90b55107d5f00fe84d9b1e2828c4
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-04 19:31:16 +0100

    Added error message for h = 0 and created error.py with error function
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Differentiation.py
+++ Differentiation.py
@@ -2,6 +2,7 @@
 This is a collection of functions related to differentiation
 """
 from decimal import *
+from error import *
 
 getcontext().rounding = ROUND_HALF_UP
 getcontext().prec = 64
@@ -16,6 +17,9 @@ def derivative(function, x, h=Decimal(0.0000000000010000000)):
     :param h: int, float, Decimal
     :return: Decimal
     """
+    if h == 0:
+        error('h cannot be 0')
+        raise DivisionByZero
     f_of_x = function(Decimal(x))
     f_of_x_plus_h = function(x + h)
     dy_dx = (f_of_x_plus_h - f_of_x) / h
